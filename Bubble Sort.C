99e3696d14f1b0d406629c0e7e7046bf2f82e2e7
Sarath-Kumar-S <54178445+Sarath-Kumar-S@users.noreply.github.com>
2019-09-26 20:14:37 +0530

    Create Bubble Sort.C

---
+++ Bubble Sort.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+	int a[10],i,j,temp,n;
+	clrscr();
+
+	printf("\n\nEnter limit: ");
+	scanf("%d",&n);
+	printf("\nEnter array:\n");
+
+	for(i=0;i<n;i++){
+		printf("\t    ");
+		scanf("%d",&a[i]);
+	}
+
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-i-1;j++){
+			if(a[j]>a[j+1]){
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+
+	printf("\nSorted Array:");
+	for(i=0;i<n;i++){
+		printf("\n\t    %d",a[i]);
+	}
+
+	getch();
+}
