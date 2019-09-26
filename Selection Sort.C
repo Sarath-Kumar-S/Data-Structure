a7099822715d81cdb73bc1dd2b2eef879d4fa819
Sarath-Kumar-S <54178445+Sarath-Kumar-S@users.noreply.github.com>
2019-09-26 20:21:35 +0530

    Create Selection Sort.C

---
+++ Selection Sort.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[10],i,j,temp,n;
+	clrscr();
+  printf("\n\nEnter limit: ");
+	scanf("%d",&n);
+	printf("\nEnter array:\n");
+  for(i=0;i<n;i++)
+  { printf("\t");
+		scanf("%d",&a[i]);
+	}
+  for(i=0;i<n;i++)
+	{  for(j=i+1;j<n;j++)
+			{  if(a[i]>a[j])
+			   {	 temp=a[i];
+				     a[i]=a[j];
+				     a[j]=temp;
+			   }
+		  }
+	}
+  printf("\nSORTED ARRAY:");
+	for(i=0;i<n;i++)
+	{ 	printf("\n%d",a[i]);
+	}
+  getch();
+
+}
