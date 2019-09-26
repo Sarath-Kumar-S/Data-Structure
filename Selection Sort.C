8d094c197109f0c49fd9258b2165e853497351b0
Sarath Kumar S <54178445+Sarath-Kumar-S@users.noreply.github.com>
2019-09-26 22:24:44 +0530

    Update Selection Sort.C

--- Selection Sort.C
+++ Selection Sort.C
@@ -2,28 +2,28 @@
 #include<conio.h>
 void main()
 {
-	int a[10],i,j,temp,n;
-	clrscr();
+  int a[10],i,j,temp,n;
+  clrscr();
   printf("\n\nEnter limit: ");
-	scanf("%d",&n);
-	printf("\nEnter array:\n");
+  scanf("%d",&n);
+  printf("\nEnter array:\n");
   for(i=0;i<n;i++)
   { printf("\t");
-		scanf("%d",&a[i]);
-	}
+    scanf("%d",&a[i]);
+  }
   for(i=0;i<n;i++)
-	{  for(j=i+1;j<n;j++)
-			{  if(a[i]>a[j])
-			   {	 temp=a[i];
+  {  for(j=i+1;j<n;j++)
+     {  if(a[i]>a[j])
+        {  temp=a[i];
 				     a[i]=a[j];
 				     a[j]=temp;
-			   }
-		  }
-	}
+        } 
+     }
+  }
   printf("\nSORTED ARRAY:");
-	for(i=0;i<n;i++)
-	{ 	printf("\n%d",a[i]);
-	}
+  for(i=0;i<n;i++)
+  {  printf("\n%d",a[i]);
+  }
   getch();
 
 }
