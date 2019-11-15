0ee99cf7c4ea72f335196c7fe8e89db075225d84
Sarath Kumar S <54178445+Sarath-Kumar-S@users.noreply.github.com>
2019-11-15 20:17:23 +0530

    To implement merge sort using recursive algorithm

---
+++ Merge Sort.C
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+void mergesort(int a[],int i,int j);
+void merge(int a[],int i1,int j1,int i2,int j2);
+
+void main(){
+    int a[30],n,i;
+    clrscr();
+    printf("\nEnter the no. of elements: ");
+    scanf("%d",&n);
+    printf("\nEnter the array elements\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    mergesort(a,0,n-1);
+
+    printf("sorted array is: ");
+    for(i=0;i<n;i++)
+        printf("| %d",a[i]);
+    printf(" |");
+    getch();
+}
+
+void mergesort(int a[],int i,int j){
+    int mid;
+    if(i<j){
+        mid=(i+j)/2;
+        mergesort(a,i,mid);
+        mergesort(a,mid+1,j);
+        merge(a,i,mid,mid+1,j);
+    }
+}
+
+void merge(int a[],int i1,int j1,int i2,int j2){
+    int temp[50];
+    int i,j,k;
+    i=i1;
+    j=i2;
+    k=0;
+    while(i<=j1 && j<=j2)
+    {
+        if(a[i]<a[j])
+            temp[k++]=a[i++];
+        else
+            temp[k++]=a[j++];
+    }
+    while(i<=j1)
+        temp[k++]=a[i++];
+    while(j<==j2)
+        temp[k++]=a[j++];
+    for(i=i1,j=0;i<=j2;i++,j++)
+        a[i]=temp[j];        
+}
